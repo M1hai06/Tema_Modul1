b2e98d11908513e44f0de5f661f252707529143d
M1hai06 <148982922+M1hai06@users.noreply.github.com>
2023-11-26 12:24:20 +0200

    Add files via upload

---
+++ Tema/script.py
@@ -0,0 +1,17 @@
+#Deretei Luca-Mihai
+import webbrowser
+'''Deschide URL-uri in Browser'''
+url = input()
+webbrowser.open_new_tab(url)
+from urllib.request import urlopen
+'''Efectueaza cereri HTTP'''
+from bs4 import BeautifulSoup
+'''Extrage informatii din documente HTML,XML'''
+page = urlopen(url)
+link = BeautifulSoup(page, 'html.parser')
+print(link.title.string)
+
+meta_tags = link.find_all('meta')
+for tag in meta_tags:
+    if 'content' in tag.attrs:
+        print(tag.get('content'))
